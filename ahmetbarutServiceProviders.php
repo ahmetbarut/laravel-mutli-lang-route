731b21a1cf3b520d6a13376f579131884c1507ce
Ahmet Barut <ahmetbarut588@gmail.com>
2020-08-26 00:04:26 +0300

    first commit

---
+++ ahmetbarutServiceProviders.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ahmetbarut\Multilang;
+
+use Illuminate\Support\ServiceProvider;
+
+class ahmetbarutServiceProviders extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . "/routes/multi_lang.php");
+        $this->publishes([
+            __DIR__ . "./routes" => base_path("routes/"),
+        ]);
+    }
+}
---
+++ src/Route.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @author Ahmet Barut - ahmetbarut588@gmail.com
+ */
+namespace ahmetbarut\Multilang;
+
+use Exception;
+
+class Route
+{
+    /**
+     * Diziden dönen rotaları depolar | Stores the routes returned from the array
+     * @var array $routes
+     */
+    protected static array $routes;
+
+    /**
+     * Kabul edilen diller | Accepted languages
+     * @var array $acceptedLocale
+     */
+    protected static array $acceptedLocale;
+
+    /**
+     * Dil'i depolar | Stores the language
+     * @var $locale
+     */
+    public $locale;
+
+
+    /**
+     * @param $locale
+     * @return void
+     */
+    public function __construct($locale = null)
+    {
+        self::$routes = include base_path("routes/multi_lang.php");
+        self::$acceptedLocale = [
+            "tr", "en"
+        ];
+        $this->locale = $locale;
+    }
+
+    /**
+     * @param string $name
+     * @param string $locale
+     * @return string $routes
+     */
+    public function name(string $name, string $locale = null)
+    {
+        if (!in_array($this->locale, self::$acceptedLocale)) {
+            $this->locale = "en"; // Varsayılan | Default
+        }
+
+        return self::$routes[$name][$this->locale];
+    }
+}
